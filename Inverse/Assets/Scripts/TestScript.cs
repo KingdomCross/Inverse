f6ea54a485d29085016a2f5a6a8afd43fe48883a
jermon humphrey <jermon.humphrey@gmail.com>
2018-06-17 02:05:32 -0700

    items and monsters spawn

--- Inverse/Assets/Scripts/TestScript.cs
+++ Inverse/Assets/Scripts/TestScript.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour {
 
+    public GameObject keyObject;
+    public GameObject[] monsterObjects;
+    public bool isKeySwitch;
 	// Use this for initialization
 	void Start () {
         Debug.Log("OnStart");
@@ -15,18 +18,44 @@ public class TestScript : MonoBehaviour {
         
 		
 	}
-    void OnTriggerEnter2D(Collider2D trigger2D)
+    void OnTriggerStay2D(Collider2D trigger2D)
     {
         Debug.Log("Hiro");
-        if(trigger2D.gameObject.tag == "Hiro'schioce")
+        if(trigger2D.tag == "Player")
         {
-            Debug.Log("Key");
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                if (isKeySwitch)
+                    Instantiate(keyObject, transform.position, Quaternion.identity);
+                else
+                {
+                    foreach (GameObject monster in monsterObjects)
+                       Instantiate(monster, transform.position, Quaternion.identity);
+                }
+            }
+        }
+        /*if(trigger2D.gameObject.tag == "Hiro'schioce")
+        {
+            Debug.Log("Key"); 
+            if(Input.GetKeyDown(KeyCode.E))
+            {
+                Instantiate(keyObject, transform.position, Quaternion.identity);
+            }
+
         }
         if(trigger2D.gameObject.tag == "WrongMove")
         {
             Debug.Log("Monsters");
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                foreach(GameObject monster in monsterObjects)
+                {
+                    Instantiate(monster, transform.position, Quaternion.identity);
+                }
 
-        }
+            }
+        
+        }*/
 
 
 
