6a2b6edf0d0e6c4b2951cc3ef9f92da56e9b1a39
KingdomCross <alexchheng1998@yahoo.com>
2018-06-16 20:22:32 -0700

    Enemy Movement

---
+++ Inverse/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+    public float movement;
+    public float timeMovement;
+    private float maxTimeMovement;
+
+	// Use this for initialization
+	void Start () {
+        maxTimeMovement = timeMovement;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        var x = Time.deltaTime * movement;
+        transform.Translate(x, 0, 0);
+        timeMovement -= Time.deltaTime;
+        if (timeMovement <= 0)
+        {
+            movement = movement * -1;
+            timeMovement = maxTimeMovement;
+        }
+    }
+}
